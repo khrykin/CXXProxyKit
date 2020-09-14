4934790720aa3beb50121c1a8a0e89b2b10ed6cd
Dmitry Khrykin <khrykin@me.com>
2020-09-14 14:39:29 +0300

    Initial Commit

---
+++ CXXProxyKit/CXXProxyKit.h
@@ -0,0 +1,19 @@
+//
+//  CXXProxyKit.h
+//  CXXProxyKit
+//
+//  Created by Dmitry Khrykin on 14.09.2020.
+//  Copyright © 2020 Dmitry Khrykin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CXXProxyKit.
+FOUNDATION_EXPORT double CXXProxyKitVersionNumber;
+
+//! Project version string for CXXProxyKit.
+FOUNDATION_EXPORT const unsigned char CXXProxyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CXXProxyKit/PublicHeader.h>
+
+
